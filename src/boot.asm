db8b054eef74191d37f37efa02cebc3f346672e0
mardaruin <mardaruin@yandex.ru>
2025-10-03 15:35:01 +0700

    head and cylinder are changing now

--- src/boot.asm
+++ src/boot.asm
@@ -2,63 +2,82 @@
 [ORG 0x7C00]
 
 start:                                        
-	cli   
-	xor ax, ax   
-	mov ss, ax              ; SS=0x0000 (stack segment)
-	mov ds, ax              ; DS=0x0000 (data segment)
-	mov sp, 0x7C00          ; SP=0x07C0 (stack pointer)
+  cli   
+  xor ax, ax   
+  mov ss, ax              ; SS=0x0000 (stack segment)
+  mov ds, ax              ; DS=0x0000 (data segment)
+  mov sp, 0x7C00          ; SP=0x07C0 (stack pointer)
 
-	mov di, 0x7E0           ; DI=0x7E0 (целевой сегмент)
-	mov es, di              ; ES=DI (адрес, куда будем загружать данные)
-	xor bx, bx              ; BX=0x0000 (смещение внутри сегмента)
+  mov di, 0x7E0           ; DI=0x7E0 (целевой сегмент)
+  mov es, di              ; ES=DI (адрес, куда будем загружать данные)
+  xor bx, bx              ; BX=0x0000 (смещение внутри сегмента)
 
-	mov di, NUMBER_OF_SECTORS
+  mov di, NUMBER_OF_SECTORS
 
-	mov al, 1               ;1 sector
-	xor ch, ch              ;0 cylinder
-	mov cl, 2               ;2 sector (after loader)
-	xor dh, dh              ;head 0
+  mov al, 1               ;1 sector
+  xor ch, ch              ;0 cylinder
+  mov cl, 2               ;2 sector (after loader)
+  xor dh, dh              ;head 0
 
 read_loop:
-	mov ah, 0x02            ; bios функция для чтения с диска
-	int 0x13
+  mov ah, 0x02            ; bios функция для чтения с диска
+  int 0x13
 
-	jc disk_read_failed
+  jc disk_read_failed
 
-	add bx, 512
-	inc cl 
-	dec di
-	jne read_loop
+  dec di
+  jz read_end
 
-	mov si, success_msg
-	call print_string
+  inc cl
+  cmp cl, MAX_SECTORS
+  jle read_loop
+  mov cl, 1
 
-	jmp halt_and_wait
+  inc dh
+  cmp dh, MAX_HEADS
+  jle read_loop
+  xor dh, dh
+
+  inc ch
+  jmp read_loop
+
+                         
 
 disk_read_failed:
-	mov si, error_msg
-	call print_string
-	jmp halt_and_wait
+  mov si, error_msg
+  call print_string
+  jmp halt_and_wait
 
 halt_and_wait:
-	hlt
-	jmp halt_and_wait
+  hlt
+  jmp halt_and_wait
 
 print_string:
-	pusha
+  pusha
 .loop:
-	lodsb
-	or al, al
-	jz .done
-	mov ah, 0x0E
-	mov bh, 0
-	int 0x10
-	jmp .loop
+  lodsb
+  or al, al
+  jz .done
+  mov ah, 0x0E
+  mov bh, 0
+  int 0x10
+  jmp .loop
 .done:
-	popa
-	ret
+  popa
+  ret
 
-NUMBER_OF_SECTORS equ 2
+read_end:
+  mov si, success_msg
+  call print_string
+    
+  jmp halt_and_wait
+
+  
+
+NUMBER_OF_SECTORS equ 200
+MAX_SECTORS equ 18
+MAX_HEADS equ 1
+MAX_CYLINDERS equ 79
 
 success_msg db 'Data successfully loaded', 0
 error_msg db 'Disk read error.', 0
