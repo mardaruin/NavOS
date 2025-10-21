60cb7ef57511bb7fb75d72fe36985d25c12b86bb
mardaruin <mardaruin@yandex.ru>
2025-10-21 14:56:55 +0700

    volatile deleted

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 extern void halt_and_wait();
                                                                          
 void clear_screen() {
-    volatile uint16_t* video_memory = (uint16_t*)0xB8000;
+    uint16_t* video_memory = (uint16_t*)0xB8000;
     
     for(int i = 0; i < 25; ++i) {
         video_memory[4 * i] = (' ');
