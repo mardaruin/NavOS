2404484d52daa3d884bbda95f4012b0110237d2b
mardaruin <mardaruin@yandex.ru>
2025-12-07 11:07:59 +0700

    tramplin size fixed

--- src/interrupts.c
+++ src/interrupts.c
@@ -1,83 +1,80 @@
 #include "interrupts.h"
-#include "vga.h"   
- 
+#include "vga.h"
 
+#define DELAY_MS(ms)                                                           \
+  ({                                                                           \
+    volatile uint32_t delay = ms * 100000;                                     \
+    while (delay--) {                                                          \
+    }                                                                          \
+  })
 
-#define DELAY_MS(ms) ({ \
-    volatile uint32_t delay = ms * 100000; \
-    while(delay--) {} \
-})
-
-                                                                                             
 static bool has_error_code(uint8_t v) {
   switch (v) {
-    case 0x8:  // Double Fault
-    case 0xA:  // Invalid TSS
-    case 0xB:  // Segment Not Present
-    case 0xC:  // Stack-Segment Fault
-    case 0xD:  // General Protection Fault
-    case 0xE:  // Page Fault
-    case 0x11: // Alignment Check
-    case 0x15: 
-    case 0x1D:  
-    case 0x1E: 
-      return true;
-    default:
-      return false;
+  case 0x8:  // Double Fault
+  case 0xA:  // Invalid TSS
+  case 0xB:  // Segment Not Present
+  case 0xC:  // Stack-Segment Fault
+  case 0xD:  // General Protection Fault
+  case 0xE:  // Page Fault
+  case 0x11: // Alignment Check
+  case 0x15:
+  case 0x1D:
+  case 0x1E:
+    return true;
+  default:
+    return false;
   }
 }
 
 void init_idt() {
-  uint8_t* tramplins = (uint8_t *) malloc_undead(TRAMPLIN_SIZE * VECTORS_AMOUNT, 1);       
+  uint8_t *tramplins =
+      (uint8_t *)malloc_undead(TRAMPLIN_SIZE * VECTORS_AMOUNT, 1);
 
   uint32_t v;
-  for (v = 0; v < VECTORS_AMOUNT; v++) {                                         
-    
+  for (v = 0; v < VECTORS_AMOUNT; v++) {
+
     bool v_has_error_code = has_error_code(v);
 
-    uint8_t* tramplin = (uint8_t *)(tramplins + v * TRAMPLIN_SIZE);
-    uint32_t offset = 0; 
+    uint8_t *tramplin = (uint8_t *)(tramplins + v * TRAMPLIN_SIZE);
+    uint32_t offset = 0;
 
-    void* bridge_handler = v_has_error_code ? collect_context_without_error_code : collect_context;
-          
-    tramplin[offset++] = 0x6a;   // push imm8
+    void *bridge_handler =
+        v_has_error_code ? collect_context_without_error_code : collect_context;
+
+    tramplin[offset++] = 0x6a; // push imm8
     tramplin[offset++] = v;
-    tramplin[offset++] = 0xe9;   // jmp                            
-                                                         
-    uint32_t jmp_offset = (uint32_t)bridge_handler - (uint32_t)(tramplin + offset + 4);
-    *(uint32_t*)(tramplin + offset) = jmp_offset;  // collect_context relative address
+    tramplin[offset++] = 0xe9; // jmp
+
+    uint32_t jmp_offset =
+        (uint32_t)bridge_handler - (uint32_t)(tramplin + offset + 4);
+    *(uint32_t *)(tramplin + offset) =
+        jmp_offset; // collect_context relative address
 
     offset += 4;
-    while (offset < TRAMPLIN_SIZE) {
-      tramplin[offset++] = 0x90; // nop
-    }
-                                                                                 
   }
-                                                                                      
 
-  idt_descriptor* idt = malloc_undead(IDT_SIZE, IDT_ALIGNMENT);
-  for (uint16_t v = 0; v < VECTORS_AMOUNT; v++) {   
+  idt_descriptor *idt = malloc_undead(IDT_SIZE, IDT_ALIGNMENT);
+  for (uint16_t v = 0; v < VECTORS_AMOUNT; v++) {
     scroll_if_needed();
     idt[v].offset_0_15 = (uint32_t)(tramplins + TRAMPLIN_SIZE * v) & 0xffff;
     idt[v].segment_selector = 0x08;
     idt[v].reserved_32_36 = 0;
-    idt[v].gate_type = 0b110; // 0b110 - interrupt gate, 0b111 - trap gate 
-    idt[v].clear_37_39 = 0; 
+    idt[v].gate_type = 0b110; // 0b110 - interrupt gate, 0b111 - trap gate
+    idt[v].clear_37_39 = 0;
     idt[v].clear_44 = 0;
     idt[v].DPL = 0b0;
-    idt[v].P = 0b1;          // 1 - present flag
-    idt[v].offset_16_31 = ((uint32_t)(tramplins + TRAMPLIN_SIZE * v) >> 16) & 0xffff;
+    idt[v].P = 0b1; // 1 - present flag
+    idt[v].offset_16_31 =
+        ((uint32_t)(tramplins + TRAMPLIN_SIZE * v) >> 16) & 0xffff;
   }
 
   IDT idtr;
   idtr.limit = IDT_SIZE - 1;
   idtr.base = idt;
   lidt(&idtr);
-
 }
 
-
-void universal_handler(interrupt_context* context) {
+void universal_handler(interrupt_context *context) {
   kernel_panic("unhandled interrupt #%x at %x:%x\n\n"
                "Registers: \n"
                "  EAX: %x, ECX: %x, EDX: %x, EBX: %x\n"
@@ -86,11 +83,10 @@ void universal_handler(interrupt_context* context) {
                "Error code: \n"
                "  common_error_code, value: %x\n\n"
                "EFLAGS\n"
-               "  value: %x\n", context->int_vector, context->cs, context->eip, 
-               context->eax, context->ecx, context->edx, context->ebx,
-               context->esp, context->ebp, context->esi, context->edi,
-               context->ds, context->es, context->fs, context->gs,
-               context->error_code, context->eflags);  
-
-}                      
-                            
+               "  value: %x\n",
+               context->int_vector, context->cs, context->eip, context->eax,
+               context->ecx, context->edx, context->ebx, context->esp,
+               context->ebp, context->esi, context->edi, context->ds,
+               context->es, context->fs, context->gs, context->error_code,
+               context->eflags);
+}
--- src/interrupts.h
+++ src/interrupts.h
@@ -4,35 +4,34 @@
 #include "types.h"
 
 #ifndef INTERRUPTS
-  #define INTERRUPTS
+#define INTERRUPTS
 
-  #define VECTORS_AMOUNT 256
-  #define DESCRIPTOR_SIZE sizeof(idt_descriptor)
-  #define IDT_ALIGNMENT 8
-  #define IDT_SIZE (VECTORS_AMOUNT * DESCRIPTOR_SIZE)
-  #define TRAMPLIN_SIZE 8
+#define VECTORS_AMOUNT 256
+#define DESCRIPTOR_SIZE sizeof(idt_descriptor)
+#define IDT_ALIGNMENT 8
+#define IDT_SIZE (VECTORS_AMOUNT * DESCRIPTOR_SIZE)
+#define TRAMPLIN_SIZE 7
 
-
-  #pragma pack(push, 1)
+#pragma pack(push, 1)
 
 typedef struct {
-  uint16_t offset_0_15      : 16; // Offset to handler entry point [0, 15] bits
+  uint16_t offset_0_15 : 16;      // Offset to handler entry point [0, 15] bits
   uint16_t segment_selector : 16; // Code segment of handler entry point
-  uint8_t reserved_32_36    : 5;
-  uint8_t clear_37_39       : 3;
-  uint8_t gate_type         : 4;
-  uint8_t clear_44          : 1;
-  uint8_t DPL               : 2;  // Descriptor privilege level
-  uint8_t P                 : 1;  // Present flag (0 - not present, 1 - present)
-  uint16_t offset_16_31     : 16; // Offset to handler entry point [16, 31] bits     `
+  uint8_t reserved_32_36 : 5;
+  uint8_t clear_37_39 : 3;
+  uint8_t gate_type : 4;
+  uint8_t clear_44 : 1;
+  uint8_t DPL : 2;            // Descriptor privilege level
+  uint8_t P : 1;              // Present flag (0 - not present, 1 - present)
+  uint16_t offset_16_31 : 16; // Offset to handler entry point [16, 31] bits `
 } idt_descriptor;
 
 typedef struct {
   uint16_t limit : 16;
-  idt_descriptor* base;
+  idt_descriptor *base;
 } IDT;
 
-  #pragma pack(pop)
+#pragma pack(pop)
 
 typedef struct {
   uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi, eip;
@@ -46,11 +45,10 @@ typedef struct {
 extern void collect_context();
 extern void collect_context_without_error_code();
 
-extern void lidt(IDT* idt_address);
+extern void lidt(IDT *idt_address);
 
 void init_idt();
 
-void universal_handler(interrupt_context* context);
-
+void universal_handler(interrupt_context *context);
 
 #endif
